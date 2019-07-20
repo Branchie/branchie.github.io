1abbdaeca8c2d760410458c227492bf7f7c4fa55
Branchie <michaelsemps@hotmail.co.uk>
2019-07-20 23:56:38 +0100

    Restructure.

---
+++ precache-manifest.0840aebb8a0ac91666545657d1a0ed68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696eaa67db996120edeb037419634c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f86a28eda43df200e03",
+    "url": "/static/js/2.18854440.chunk.js"
+  },
+  {
+    "revision": "9ddf3b457df7d3dfe4a8",
+    "url": "/static/js/main.6de45083.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b68c4a8faf74d7ce300130108df3e75.js"
+  "/precache-manifest.0840aebb8a0ac91666545657d1a0ed68.js"
 );
 
 self.addEventListener('message', (event) => {
