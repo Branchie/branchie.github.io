8e675c1d22f1b5e4a819545bd8bfe5b2248d6732
Branchie <michaelsemps@hotmail.co.uk>
2019-07-20 16:41:39 +0100

    Updates

---
+++ precache-manifest.763a5429e04c6f9667a1e7bc44776079.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf29a795c1e3ded5b0eff80b22e092ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f86a28eda43df200e03",
+    "url": "/static/js/2.18854440.chunk.js"
+  },
+  {
+    "revision": "2005e56b7b6128ecb5db",
+    "url": "/static/js/main.b99e542e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
